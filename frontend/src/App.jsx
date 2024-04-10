a7f4eea7124c19fdad1b06eaf1eedbfce1de1b2a
Tarun Naidu G <welcometokannada2017@gmail.com>
2024-04-10 17:14:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
