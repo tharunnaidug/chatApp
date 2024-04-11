377ee0cf0842ed4f66cba8fcecee5a4479105b2d
Tarun Naidu G <welcometokannada2017@gmail.com>
2024-04-11 16:36:26 +0530

    Deployment

--- frontend/src/hooks/userSignup.js
+++ frontend/src/hooks/userSignup.js
@@ -12,7 +12,7 @@ const userSignup = () => {
     if (checkInputs) return;
 
     try {
-      const res = await fetch("http://localhost:3000/auth/register", {
+      const res = await fetch("/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, username, password, cpassword, email, gender })
