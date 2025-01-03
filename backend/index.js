03b077f315160950664af3786267e7dfae19dba4
Tarun Naidu G <welcometokannada2017@gmail.com>
2025-01-03 22:19:03 +0530

    Update index.js
    According to Deployment Needs

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import path from 'path';
 import express from 'express';
 import { config } from 'dotenv';
-import { connetDb } from './db/connecttodb.js';
+//import connetDb from './db/connecttodb.js';
 import authroutes from './routes/auth.routes.js';
 import msgroutes from './routes/msg.routes.js';
 import userroutes from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import { app, server } from './socket/socket.js';
+import mongoose from "mongoose";
 
 
 const __dirname=path.resolve()
@@ -37,6 +38,21 @@ app.use(express.static(path.join(__dirname,'/frontend/dist')));
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+const connetDb=async ()=>{
+
+    try {
+      await  mongoose.connect(process.env.MONGODB_URL)
+        console.log("Connected to DB")
+    } catch (error) {
+        console.log("There was Some Problem conneting to DB ",error )
+    }
+}
+
+server.listen(port, () => { 
+    connetDb()
+    console.log(`Listening on Port :${port}`)
 })
 
 server.listen(port, () => { 
